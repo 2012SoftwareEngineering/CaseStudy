daafb473621bf5403615e7af6dc631d6fd157117
evil4t <wakeupevil@gmail.com>
2014-02-20 21:35:52 +0800

    Demo

--- DemoOne/src/com/example/demoone/EditActivity.java
+++ DemoOne/src/com/example/demoone/EditActivity.java
@@ -2,6 +2,8 @@ package com.example.demoone;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -17,17 +19,25 @@ public class EditActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initView();
-		
+
 	}
-	
-	private void initView()
-	{
+
+	private void initView() {
 		edit_userName = (EditText) findViewById(R.id.edit_userName);
 		edit_userPhone = (EditText) findViewById(R.id.edit_userPhone);
 		edit_userEmail = (EditText) findViewById(R.id.edit_userEmail);
 		edit_userProfessional = (EditText) findViewById(R.id.edit_userProfessional);
 		imageBtn_save = (ImageButton) findViewById(R.id.imageBtn_save);
+
+		imageBtn_save.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+                 
+			}
+		});
+
 	}
-	
-	
+
 }
