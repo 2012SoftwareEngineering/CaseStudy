ef6c3a43760be61a25cb6bb1e2b3cb7314b6d5c2
szp <szpssky@gmail.com>
2014-02-20 21:42:22 +0800

    demo

--- DemoOne/src/com/example/demoone/EditActivity.java
+++ DemoOne/src/com/example/demoone/EditActivity.java
@@ -1,7 +1,11 @@
 package com.example.demoone;
 
+import domain.businessEntity.systemManagement.User;
+import domain.businessService.systemManagement.ISystemManagementService;
+import domain.businessService.systemManagement.SystemManagementService;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -11,13 +15,31 @@ public class EditActivity extends Activity {
 			edit_userEmail, edit_userQQ, edit_userProfessional,
 			edit_userAddress;
 	private ImageButton imageBtn_save;
-
+	private ISystemManagementService service;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//实例化SystemManagementServcie
+		service = new SystemManagementService();
+		//初始化view
 		initView();
-		
+		/**
+		 * 监听保存键单击响应情况
+		 */
+		imageBtn_save.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				User user = new User();
+				//使用getText方法获取编辑框控件的内容
+				user.setUsername(edit_userName.getText().toString());
+				user.setUserPhone(edit_userPhone.getText().toString());
+				user.setEmail(edit_userEmail.getText().toString());
+				user.setUserProfessional(edit_userProfessional.getText().toString()); 
+				
+			}
+		});
 	}
 	
 	private void initView()
